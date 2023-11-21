171fb72fad9370afb7bf5a85f7e40eb4e3c03af9
NeuralNat <ifrance@yandex.ru>
2023-11-21 21:27:34 +0300

    CSS dark mode colors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 btnDarkMode.onclick = function(){
     btnDarkMode.classList.toggle('dark-mode-btn--active');
+    document.body.classList.toggle('dark');
 };
