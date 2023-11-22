692b6c5c1685129d9c0ad35b01b9f45615996c0e
NeuralNat <ifrance@yandex.ru>
2023-11-22 21:20:04 +0300

    JS: If the system settings change, change the theme

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
-const btnDarkMode = document.querySelector('.dark-mode-btn');
+const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// Проверка темной темы в Local storage
-if (localStorage.getItem('darkMode') === 'dark') {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
-};
+// // Проверка темной темы в Local storage
+// if (localStorage.getItem('darkMode') === 'dark') {
+//     btnDarkMode.classList.add("dark-mode-btn--active");
+//     document.body.classList.add("dark");
+// };
 
 // Проверка темной темы на уровне системных настроек
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
 }
 
+//Если меняются системные настройки - меняем тему
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+    const newColorScheme = event.macthes ? "dark" : "light";
+    
+
+    if (newColorScheme === dark) {
+      btnDarkMode.classList.add("dark-mode-btn--active");
+      document.body.classList.add("dark");
+    } else {
+      btnDarkMode.classList.remove("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+    }
+  });
 
 // Включение ночного режима по кнопке
-btnDarkMode.onclick = function(){
-    btnDarkMode.classList.toggle('dark-mode-btn--active');
-    const isDark = document.body.classList.toggle('dark');
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark");
 
-    if(isDark) {
-        localStorage.setItem('darkMode', 'dark');
-    } else {
-        localStorage.setItem('darkMode', 'light')
-    }
-
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
 };
