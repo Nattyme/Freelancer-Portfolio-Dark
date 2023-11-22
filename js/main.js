c92fca2819386fed1f909d85906a6c8529510344
NeuralNat <ifrance@yandex.ru>
2023-11-22 21:33:29 +0300

    JS: order

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// // Проверка темной темы в Local storage
-// if (localStorage.getItem('darkMode') === 'dark') {
-//     btnDarkMode.classList.add("dark-mode-btn--active");
-//     document.body.classList.add("dark");
-// };
-
-// Проверка темной темы на уровне системных настроек
+// 1. Проверка темной темы на уровне системных настроек
 if (
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -15,21 +9,16 @@ if (
   document.body.classList.add("dark");
 }
 
-//Если меняются системные настройки - меняем тему
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
-    const newColorScheme = event.macthes ? "dark" : "light";
-    
+// 2.  Проверка темной темы в local storage
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+  btnDarkMode.classList.remove("dark-mode-btn--active");
+  document.body.classList.remove("dark");
+}
 
-    if (newColorScheme === dark) {
-      btnDarkMode.classList.add("dark-mode-btn--active");
-      document.body.classList.add("dark");
-    } else {
-      btnDarkMode.classList.remove("dark-mode-btn--active");
-      document.body.classList.remove("dark");
-    }
-  });
-
-// Включение ночного режима по кнопке
+// 3. Включение ночного режима по кнопке
 btnDarkMode.onclick = function () {
   btnDarkMode.classList.toggle("dark-mode-btn--active");
   const isDark = document.body.classList.toggle("dark");
@@ -40,3 +29,32 @@ btnDarkMode.onclick = function () {
     localStorage.setItem("darkMode", "light");
   }
 };
+
+// 3. Если меняются системные настройки - меняем тему
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.macthes ? "dark" : "light";
+
+    if (newColorScheme === dark) {
+      btnDarkMode.classList.add("dark-mode-btn--active");
+      document.body.classList.add("dark");
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      btnDarkMode.classList.remove("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkMode", "li");
+    }
+  });
+
+  // 4. Включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+  
+    if (isDark) {
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      localStorage.setItem("darkMode", "light");
+    }
+  };
